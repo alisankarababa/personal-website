fcbfa46735558d6c801dd1fd8fddd1ac2c8482be
Alişan Karababa <alisankarababa@gmail.com>
2023-11-06 13:20:41 +0300

    update Profile
    add about me section
    
    add css rules for layout

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,7 +4,7 @@ import { page_en } from "../../data/data";
 
 const { profileData } = page_en;
 
-const { basicInfo } = profileData;
+const { basicInfo, aboutMe } = profileData;
 
 export default function Profile() {
 	return (
@@ -12,17 +12,23 @@ export default function Profile() {
 			<section className="sec-profile custom-container">
 				<h2 className="sec--title" >{profileData.title}</h2>
 				<div className="profile">
-					<div className="profile__basic-info basic-info">
+					<div className="basic-info profile__item">
                         <h3 className="basic-info__title" style={basicInfo.title.style}>{basicInfo.title.text}</h3>
-                        <div className="basic-info__items">
                         {
                             basicInfo.info.map( info => {
-                                return <div key={info.label} className="basic-info__item"><label>{info.label}</label><span>{info.text}</span></div>
+                                return <div key={info.label} className="basic-info__option"><label className="basic-info__label">{info.label}</label>
+                                <span className="basic-info__text">{info.text}</span></div>
                             })
                         }
-                        </div>
                     </div>
-					<div className="profile__about-me"></div>
+					<div className="profile__about-me profile__item">
+                        <h3 className="about-me__title">{aboutMe.title}</h3>
+                        {
+                            aboutMe.paragraphs.map( paragraph => (
+                                <p className="about-me__paragraphs" key={paragraph}>{paragraph}</p>
+                            ) )
+                        }
+                    </div>
 				</div>
 			</section>
 		</div>
