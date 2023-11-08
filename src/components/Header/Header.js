64b03dc5cf188a8b3564161c3869b1a1be1760ae
Alişan Karababa <alisankarababa@gmail.com>
2023-11-08 13:45:56 +0300

    update Header
    add :hover -> cursor: pointer to language name
    update font weight, font family
    add padding to Header
    add text to tell which the to switch if switch is clicked, style it
    , text changes based on language
    
    add color to LanguageSelect

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,58 @@
 import "./Header.css";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import { useLanguage } from "../../contexts/ContextLanguage";
+import { useTheme } from "../../contexts/ContextTheme";
 
 export default function Header() {
 
     const { eLanguage, language, switchLanguage } = useLanguage();
+    const { eTheme, theme } = useTheme();
 
     function LanguageSelect() {
 
         switch (language) {
             case eLanguage.tr:
-                return <span><span onClick={() => switchLanguage(eLanguage.en)} className="clr-1769FF">İNGİLİZCE</span>' YE GEÇ </span>
+                return <span className="clr-777777"><span onClick={() => switchLanguage(eLanguage.en)} className="clr-1769FF language-select">İNGİLİZCE</span>' YE GEÇ </span>
             case eLanguage.en:
-                return <span>SWITCH TO <span onClick={() => switchLanguage(eLanguage.tr)} className="clr-1769FF">TURKISH</span></span> 
+                return <span className="clr-777777">SWITCH TO <span onClick={() => switchLanguage(eLanguage.tr)} className="clr-1769FF language-select">TURKISH</span></span> 
             default:
                 return <span></span>;
         }
     }
 
+    function ThemeText() {
+
+        switch (language) {
+            case eLanguage.tr:
+                return <span className={ eTheme.dark === theme ? "clr-grayD9": "clr-777777"}> 
+                {
+                    eTheme.dark === theme ? "GÜNDÜZ MODU" : "GECE MODU"
+                }
+                </span>
+
+            case eLanguage.en:
+                return <span className={ eTheme.dark === theme ? "clr-grayD9": "clr-777777"}> 
+                {
+                    eTheme.dark === theme ? "LIGHT MODE" : "DARK MODE"
+                }
+                </span>
+        
+            default:
+                return <span></span>
+        }
+        
+        
+    }
+
 	return (
-        <header className="header">
-            <ToggleSwitch />
+        <div className="header-wrapper">
+        <header className="header custom-container">
+            <div className="header__theme">
+                <ToggleSwitch />
+                <ThemeText />
+            </div>
             <LanguageSelect />
         </header>
+        </div>
     );
 }
