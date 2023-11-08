da2e97019dcf2e16834b02ac41f27c6a744288b8
Alişan Karababa <alisankarababa@gmail.com>
2023-11-08 12:26:37 +0300

    add language switch to Header
    switch message changes depending on the language e.g. if language
    
    is english it says "SWITCH TO TURKISH", if language is turkish it says
    
    "İNGİLİZCE' YE GEÇ"

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,27 @@
 import "./Header.css";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import { useLanguage } from "../../contexts/ContextLanguage";
 
 export default function Header() {
 
-	return <ToggleSwitch />;
+    const { eLanguage, language, switchLanguage } = useLanguage();
+
+    function LanguageSelect() {
+
+        switch (language) {
+            case eLanguage.tr:
+                return <span><span onClick={() => switchLanguage(eLanguage.en)} className="clr-1769FF">İNGİLİZCE</span>' YE GEÇ </span>
+            case eLanguage.en:
+                return <span>SWITCH TO <span onClick={() => switchLanguage(eLanguage.tr)} className="clr-1769FF">TURKISH</span></span> 
+            default:
+                return <span></span>;
+        }
+    }
+
+	return (
+        <header className="header">
+            <ToggleSwitch />
+            <LanguageSelect />
+        </header>
+    );
 }
